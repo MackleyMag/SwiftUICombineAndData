ed70fedf5a355957ff29747abf85c74624ae5ca0
Mackley <mackleymagalhaess@hotmail.com>
2023-10-16 22:35:53 -0400

    Initial Commit

---
+++ SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by Mackley Magalhães da Silva on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
